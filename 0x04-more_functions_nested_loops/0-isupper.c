515663874c130725aefc43c277fa881322acb89a
Shedih1 <shadracktulutian@gmail.com>
2022-12-25 12:05:38 -0500

    Message: Initial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * _isupper - checks for uppercase characters
+ * @c : char
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+int i;
+
+for (i = 'A'; i <= 'Z'; i++)
+{
+if (i == c)
+{
+return (1);
+}
+}
+return (0);
+}
